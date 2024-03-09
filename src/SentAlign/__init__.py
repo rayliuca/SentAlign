728dea26de57a02c88a871f9087c0026a7164b2f
Ray <ray@rayliu.ca>
2024-03-09 14:06:46 -0700

    minor clean up

--- src/SentAlign/__init__.py
+++ src/SentAlign/__init__.py
@@ -3,13 +3,7 @@ from pathlib import Path
 
 __all__ = []
 
-# for f in list(Path(__file__).parent.glob("*.pyx")) + list(Path(__file__).parent.glob("*.py")):
 for f in list(Path(__file__).parent.glob("*.py")):
-# pyx_files = ['buffer_work_space.pyx', 'anchoring.pyx', 'file_read_back.pyx', 'galechurch.pyx', 'greedy.pyx',
-#              'utilities.pyx']
-#
-# for f in [Path(__file__).parent / f for f in pyx_files] + list(Path(__file__).parent.glob("*.py")):
-    print(f)
     module_name = f.stem
     if (not module_name.startswith("_")) and (module_name not in globals()):
         import_module(f".{module_name}", __package__)
